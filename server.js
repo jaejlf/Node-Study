8daded16b4f605f6c3525b245f8faa932a9f45ef
jaejeong <78673570+jaejlf@users.noreply.github.com>
2021-10-16 02:48:22 +0900

    Setting up mongodb

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(express.urlencoded({extended : true}));
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(8080, function () {
-    console.log('listening on 8080')
-});
+MongoClient.connect('mongodb+srv://id:<password>@cluster0.h0sge.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function (에러, client) {
+    if (에러) return console.log(에러)
+    app.listen(8080, function () {
+        console.log('listening on 8080')
+    })
+})
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
@@ -15,7 +20,7 @@ app.get('/write', function (req, res) {
     res.sendFile(__dirname + '/write.html')
 });
 
-app.post('/add', function (req, res){
+app.post('/add', function (req, res) {
     res.send('전송완료');
     console.log(req.body);
 });
