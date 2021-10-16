48d4803c972042a210e88cd12a815a2a528658f9
jaejeong <78673570+jaejlf@users.noreply.github.com>
2021-10-17 02:14:20 +0900

    Auto increment practice

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 
 var db;
 MongoClient.connect('mongodb+srv://id:<password>@cluster0.h0sge.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (에러, client) {
-    if (에러) return console.log(에러)
+    if (에러) return console.log(에러);
     db = client.db('todoapp');
     app.listen(8080, function () {
         console.log('listening on 8080')
@@ -32,8 +32,14 @@ app.get('/list', function (req, res) {
 });
 
 app.post('/add', function (req, res) {
-    res.send('전송완료');
-    db.collection('post').insertOne({ 제목: req.body.title, 날짜: req.body.date }, function () {
-        console.log('저장완료')
+    db.collection('counter').findOne({ name: '게시물 갯수' }, function (에러, 결과) {
+        var count = 결과.totalPost;
+
+        db.collection('post').insertOne({ _id: count + 1, 제목: req.body.title, 날짜: req.body.date }, function (에러, 결과) {
+            db.collection('counter').updateOne({ name: '게시물 갯수' }, { $inc: { totalPost: 1 } }, function (에러, 결과) {
+                if (에러) return console.log(에러);
+                res.send('전송완료');
+            })
+        });
     });
 });
