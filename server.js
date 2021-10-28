c9b7e7eee8630a090f0051436902a650c4608937
jaejeong <78673570+jaejlf@users.noreply.github.com>
2021-10-29 01:58:21 +0900

    Modify wording

--- server.js
+++ server.js
@@ -16,46 +16,50 @@ MongoClient.connect('mongodb+srv://id:<password>@cluster0.h0sge.mongodb.net/myFi
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
+app.get('/', function (요청, 응답) {
+    응답.render('index.ejs');
 });
 
-app.get('/write', function (req, res) {
-    res.sendFile(__dirname + '/write.html')
+app.get('/write', function (요청, 응답) {
+    응답.render('write.ejs');
 });
 
-app.get('/list', function (req, res) {
+app.get('/list', function (요청, 응답) {
     db.collection('post').find().toArray(function (에러, 결과) {
         console.log(결과);
-        res.render('list.ejs', { posts: 결과 });
+        응답.render('list.ejs', { posts: 결과 });
     })
 });
 
-app.post('/add', function (req, res) {
+app.post('/add', function (요청, 응답) {
     db.collection('counter').findOne({ name: '게시물 갯수' }, function (에러, 결과) {
         var count = 결과.totalPost;
 
-        db.collection('post').insertOne({ _id: count + 1, 제목: req.body.title, 날짜: req.body.date }, function (에러, 결과) {
+        db.collection('post').insertOne({ _id: count + 1, 제목: 요청.body.title, 날짜: 요청.body.date }, function (에러, 결과) {
             db.collection('counter').updateOne({ name: '게시물 갯수' }, { $inc: { totalPost: 1 } }, function (에러, 결과) {
                 if (에러) return console.log(에러);
-                res.send('전송완료');
+                응답.send('전송완료');
             })
         });
     });
 });
 
-app.delete('/delete', function (req, res) {
-    req.body._id = parseInt(req.body._id);
-    db.collection('post').deleteOne(req.body, function (에러, 결과) {
-        db.collection('counter').updateOne({ name: '게시물 갯수' }, { $inc: { totalPost: -1 } }, function (에러, 결과) {});
+app.delete('/delete', function (요청, 응답) {
+    요청.body._id = parseInt(요청.body._id);
+    db.collection('post').deleteOne(요청.body, function (에러, 결과) {
+        db.collection('counter').updateOne({ name: '게시물 갯수' }, { $inc: { totalPost: -1 } }, function (에러, 결과) { });
         console.log('삭제완료');
-        res.status(200).send({ message: '성공' });
+        응답.status(200).send({ message: '성공' });
     });
 })
 
-app.get('/detail/:id', function (req, res) {
-    db.collection('post').findOne({ _id: parseInt(req.params.id) }, function (에러, 결과) {
+app.get('/detail/:id', function (요청, 응답) {
+    db.collection('post').findOne({ _id: parseInt(요청.params.id) }, function (에러, 결과) {
         console.log(결과);
-        req.render('detail.ejs', { data: 결과 });
+        응답.render('detail.ejs', { data: 결과 });
     });
+})
+
+app.get('/edit', function (요청, 응답) {
+    응답.render('edit.ejs');
 })
