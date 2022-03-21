46a482370cee37ecdcd747d57b7fb4b6b8ae92b7
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-03-21 16:29:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 
 var db;
-MongoClient.connect(process.env.MONGODB_URL, { useUnifiedTopology: true }, function (에러, client) {
-    if (에러) return console.log(에러);
+MongoClient.connect(process.env.MONGODB_URL, { useUnifiedTopology: true }, function (err, client) {
+    if (err) return console.log(err);
     db = client.db("todoapp");
     app.listen(8080, function () {
         console.log("listening on 8080");
@@ -49,96 +49,96 @@ MongoClient.connect(process.env.MONGODB_URL, { useUnifiedTopology: true }, funct
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", function (요청, 응답) {
-    응답.render("index.ejs");
+app.get("/", function (req, res) {
+    res.render("index.ejs");
 });
 
-app.get("/write", function (요청, 응답) {
-    응답.render("write.ejs");
+app.get("/write", function (req, res) {
+    res.render("write.ejs");
 });
 
-app.get("/list", function (요청, 응답) {
+app.get("/list", function (req, res) {
     db.collection("post")
         .find()
-        .toArray(function (에러, 결과) {
-            console.log(결과);
-            응답.render("list.ejs", { posts: 결과 });
+        .toArray(function (err, result) {
+            console.log(result);
+            res.render("list.ejs", { posts: result });
         });
 });
 
-app.post("/add", function (요청, 응답) {
-    db.collection("counter").findOne({ name: "게시물 갯수" }, function (에러, 결과) {
-        var count = 결과.totalPost;
+app.post("/add", function (req, res) {
+    db.collection("counter").findOne({ name: "게시물 갯수" }, function (err, result) {
+        var count = result.totalPost;
 
-        db.collection("post").insertOne({ _id: count + 1, 제목: 요청.body.title, 날짜: 요청.body.date }, function (에러, 결과) {
-            db.collection("counter").updateOne({ name: "게시물 갯수" }, { $inc: { totalPost: 1 } }, function (에러, 결과) {
-                if (에러) return console.log(에러);
-                응답.send("전송완료");
+        db.collection("post").insertOne({ _id: count + 1, 제목: req.body.title, 날짜: req.body.date }, function (err, result) {
+            db.collection("counter").updateOne({ name: "게시물 갯수" }, { $inc: { totalPost: 1 } }, function (err, result) {
+                if (err) return console.log(err);
+                res.send("전송완료");
             });
         });
     });
 });
 
-app.delete("/delete", function (요청, 응답) {
-    요청.body._id = parseInt(요청.body._id);
-    db.collection("post").deleteOne(요청.body, function (에러, 결과) {
-        db.collection("counter").updateOne({ name: "게시물 갯수" }, { $inc: { totalPost: -1 } }, function (에러, 결과) {});
+app.delete("/delete", function (req, res) {
+    req.body._id = parseInt(req.body._id);
+    db.collection("post").deleteOne(req.body, function (err, result) {
+        db.collection("counter").updateOne({ name: "게시물 갯수" }, { $inc: { totalPost: -1 } }, function (err, result) {});
         console.log("삭제완료");
-        응답.status(200).send({ message: "성공" });
+        res.status(200).send({ message: "성공" });
     });
 });
 
-app.get("/detail/:id", function (요청, 응답) {
-    db.collection("post").findOne({ _id: parseInt(요청.params.id) }, function (에러, 결과) {
-        console.log(결과);
-        응답.render("detail.ejs", { data: 결과 });
+app.get("/detail/:id", function (req, res) {
+    db.collection("post").findOne({ _id: parseInt(req.params.id) }, function (err, result) {
+        console.log(result);
+        res.render("detail.ejs", { data: result });
     });
 });
 
-app.get("/edit/:id", function (요청, 응답) {
-    db.collection("post").findOne({ _id: parseInt(요청.params.id) }, function (에러, 결과) {
-        console.log(결과);
-        응답.render("edit.ejs", { post: 결과 });
+app.get("/edit/:id", function (req, res) {
+    db.collection("post").findOne({ _id: parseInt(req.params.id) }, function (err, result) {
+        console.log(result);
+        res.render("edit.ejs", { post: result });
     });
 });
 
-app.put("/edit", function (요청, 응답) {
-    db.collection("post").updateOne({ _id: parseInt(요청.body.id) }, { $set: { 제목: 요청.body.title, 날짜: 요청.body.date } }, function (에러, 결과) {
+app.put("/edit", function (req, res) {
+    db.collection("post").updateOne({ _id: parseInt(req.body.id) }, { $set: { 제목: req.body.title, 날짜: req.body.date } }, function (err, result) {
         console.log("수정완료");
-        응답.redirect("/list");
+        res.redirect("/list");
     });
 });
 
-app.get("/upload", function (요청, 응답) {
-    응답.render("upload.ejs");
+app.get("/upload", function (req, res) {
+    res.render("upload.ejs");
 });
 
-app.post("/upload", upload.single("profileImg"), function (요청, 응답) {
-    응답.send("업로드 완료");
+app.post("/upload", upload.single("profileImg"), function (req, res) {
+    res.send("업로드 완료");
 });
 
-app.get("/image/:imgName", function (요청, 응답) {
-    응답.sendFile(__dirname + "/pulic/image/" + 요청.params.imgName);
+app.get("/image/:imgName", function (req, res) {
+    res.sendFile(__dirname + "/pulic/image/" + req.params.imgName);
 });
 
-app.get("/login", function (요청, 응답) {
-    응답.render("login.ejs");
+app.get("/login", function (req, res) {
+    res.render("login.ejs");
 });
 
-app.post("/login", passport.authenticate("local", { failureRedirect: "/fail" }), function (요청, 응답) {
-    응답.redirect("/");
+app.post("/login", passport.authenticate("local", { failureRedirect: "/fail" }), function (req, res) {
+    res.redirect("/");
 });
 
-app.get("/mypage", 로그인했니, function (요청, 응답) {
-    console.log(요청.user);
-    응답.render("mypage.ejs", { 사용자: 요청.user });
+app.get("/mypage", checkAuth, function (req, res) {
+    console.log(req.user);
+    res.render("mypage.ejs", { 사용자: req.user });
 });
 
-function 로그인했니(요청, 응답, next) {
-    if (요청.user) {
+function checkAuth(req, res, next) {
+    if (req.user) {
         next();
     } else {
-        응답.send("로그인X");
+        res.send("로그인X");
     }
 }
 
@@ -150,13 +150,13 @@ passport.use(
             session: true,
             passReqToCallback: false,
         },
-        function (입력한아이디, 입력한비번, done) {
-            //console.log(입력한아이디, 입력한비번);
-            db.collection("login").findOne({ id: 입력한아이디 }, function (에러, 결과) {
-                if (에러) return done(에러);
-                if (!결과) return done(null, false, { message: "존재하지않는 아이디" });
-                if (입력한비번 == 결과.pw) {
-                    return done(null, 결과);
+        function (inputID, inputPW, done) {
+            //console.log(inputID, inputPW);
+            db.collection("login").findOne({ id: inputID }, function (err, result) {
+                if (err) return done(err);
+                if (!result) return done(null, false, { message: "존재하지않는 id" });
+                if (inputPW == result.pw) {
+                    return done(null, result);
                 } else {
                     return done(null, false, { message: "비번틀렸어요" });
                 }
@@ -171,8 +171,8 @@ passport.serializeUser(function (user, done) {
 });
 
 //마이페이지 접속 시 실행 (user 누군지 찾는)
-passport.deserializeUser(function (아이디, done) {
-    db.collection("login").findOne({ id: 아이디 }, function (에러, 결과) {
-        done(null, 결과);
+passport.deserializeUser(function (id, done) {
+    db.collection("login").findOne({ id: id }, function (err, result) {
+        done(null, result);
     });
 });
